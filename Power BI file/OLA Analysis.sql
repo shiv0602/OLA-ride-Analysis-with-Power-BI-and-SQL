8ba78c7bcaf5c6d58f5b71c2dca3eaf64e9f6cbd
SHIV KUMAR RANA <shivkrana1998@gmail.com>
2025-02-04 22:56:42 +0530

    Add files via upload

---
+++ Power BI file/OLA Analysis.sql
@@ -0,0 +1,38 @@
+CREATE TABLE OLA(
+Date_ DATE,
+Time_ TIME,
+Booking_ID VARCHAR(100),
+Booking_Status VARCHAR(100),
+Customer_ID VARCHAR(100),
+Vehicle_Type VARCHAR(100),
+Pickup_Location VARCHAR(100),
+Drop_Location VARCHAR(100),
+V_TAT VARCHAR(100),
+C_TAT VARCHAR(100),
+Canceled_Rides_by_Customer VARCHAR(100),
+Canceled_Rides_by_Driver VARCHAR(100),
+Incomplete_Rides VARCHAR(100),
+Incomplete_Rides_Reason VARCHAR(100),
+Booking_Value int,
+Payment_Method VARCHAR(100),
+Ride_Distance varchar(100),
+Driver_Ratings varchar(100),
+Customer_Rating VARCHAR(100)
+)
+
+select * from ola
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
